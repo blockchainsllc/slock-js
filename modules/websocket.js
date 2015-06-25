6f34826ac99eca2c1ab835003c723e645919efc7
Simon Jentzsch <simon@jentzschfamily.net>
2015-06-25 08:42:05 +0200

    refactored modules

--- modules/websocket.js
+++ modules/websocket.js
@@ -1,49 +1,38 @@
 var WebSocketClient = require('websocket').client;
-function Client(config,events, id) {
-	this.id=id;
-	var _=this;
-	
-	function connect() {
-		var client = new WebSocketClient();
-		 
-		client.on('connectFailed', function(error) {
-		    console.log('connectFailed: ' + error.toString());
-	        if (client.tryReconnect) setTimeout(client.tryReconnect, 5000);	
-		});
-		 
-		client.on('connect', function(connection) {
-		    console.log('WebSocket Client Connected :'+id);
-		    connection.on('error', function(error) {
-		        console.log("Connection Error: " + error.toString());
-		        if (client.tryReconnect) setTimeout(client.tryReconnect, 5000);	
-		    });
-		    connection.on('close', function() {
-		        console.log('echo-protocol Connection Closed '+id);
-		    });
-		    connection.on('message', function(message) {
-		        if (message.type === 'utf8') {
-		            console.log(id+" Received: '" + message.utf8Data + "'");
-		            events.emit("message",JSON.parse(message.utf8Data));
-		        }
-		    });
+module.exports = function() {
+	var events = this.events, wsConfig = this.config.modules.websocket;
+	events.on("watchContract", function(contract) {
+		var id = contract.config.adr;
 
-		    connection.sendUTF(JSON.stringify({
-		    	cmd : "init",
-		    	adr : id
-		    }));
-		});
-		client.tryReconnect=connect;
-		client.connect('ws://'+config.host+':'+config.port+'/', 'echo-protocol');
-	}
+		function connect() {
+			var client = new WebSocketClient();
+			 
+			client.on('connectFailed', function(error) {
+			    console.log('connectFailed: ' + error.toString());
+		            setTimeout(connect, 5000);	
+			});
+			 
+			client.on('connect', function(connection) {
+			    console.log('WebSocket Client Connected :'+id);
+			    connection.on('error', function(error) {
+			        console.log("Connection Error: " + error.toString());
+			        setTimeout(connect, 5000);	
+			    });
+			    connection.on('close', function() {   console.log('echo-protocol Connection Closed '+id);   });
+			    connection.on('message', function(message) {
+			        if (message.type === 'utf8') {
+			            console.log(id+" Received: '" + message.utf8Data + "'");
+			            events.emit("message",JSON.parse(message.utf8Data));
+			        }
+			    });
 	
-	connect();
-}
-
-function Sockets() {
-	var _ = this;
-	this.clients = {};
-	this.events.on("watchContract", function(contract) {
-		_.clients[contract.config.adr]= new Client(_.config.modules.websocket,_.events,contract.config.adr);
+	                    // register with its id in order to be able to get messages
+			    connection.sendUTF(JSON.stringify({	cmd : "init", 	adr : id   }));
+			});
+			client.connect('ws://'+wsConfig.host+':'+wsConfig.port+'/', 'echo-protocol');
+		}
+		
+		// connect to ws-server
+		connect();
 	});
-}
-module.exports = Sockets;
+};
