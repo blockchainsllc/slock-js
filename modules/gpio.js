c7608255d437bc2ce02f151e6d3378f09d6ac437
Simon Jentzsch <simon@jentzschfamily.net>
2015-07-23 11:54:04 +0200

    fixed formating and moved tests

--- modules/gpio.js
+++ modules/gpio.js
@@ -1,4 +1,4 @@
-var fs = require("fs");
+var fs       = require("fs");
 var gpioPath = '/sys/class/gpio/';
 
 /**
@@ -8,11 +8,11 @@ var gpioPath = '/sys/class/gpio/';
  * 
  */
 function setGpioValue(pin, param, value, runAfter) {
-     fs.writeFile(gpioPath+"gpio"+pin+"/"+param,value, function(err){
-		 if (err) throw "Error trying to set the value for "+param+" of pin "+pin+" to "+value+" : "+err;
-		 console.log("Changed "+param+" of Pin "+pin+" to "+value);
-		 if (runAfter) runAfter();
-	 });	   
+  fs.writeFile(gpioPath + "gpio" + pin + "/" + param, value, function (err) {
+    if (err) throw "Error trying to set the value for " + param + " of pin " + pin + " to " + value + " : " + err;
+    console.log("Changed " + param + " of Pin " + pin + " to " + value);
+    if (runAfter) runAfter();
+	 });
 }
 
 /**
@@ -20,34 +20,34 @@ function setGpioValue(pin, param, value, runAfter) {
  * and setting the direction to out
  */
 function initPin(pin, runAfter) {
-   	fs.writeFile(gpioPath+"export",pin, function(err){
-		 if (err) throw "Error trying to create the pin "+pin+":"+err;
-		 setGpioValue(pin,"direction","out", runAfter);
-	});	
+  fs.writeFile(gpioPath + "export", pin, function (err) {
+    if (err) throw "Error trying to create the pin " + pin + ":" + err;
+    setGpioValue(pin, "direction", "out", runAfter);
+  });
 }
 
 /**
  * checks if the pin was exported and initializes if not
  */
 function checkPin(pin, runAfter) {
-  	fs.exists(gpioPath+"gpio"+pin+"/value",function(exists){
-	     if (exists) 
+  fs.exists(gpioPath + "gpio" + pin + "/value", function (exists) {
+    if (exists)
 		    runAfter();
-		 else 
-		    initPin(pin,runAfter);
-	});
+    else
+		    initPin(pin, runAfter);
+  });
 }
 
-module.exports = function() {
-	this.init = function(arg) {
-	     console.log("Start GPIO ...");
-	     this.events.on('changeState',function(arg) {
-		 if (arg.config.gpio) {
-        console.log('GPIO : open '+arg.id+"="+arg.open);
-				checkPin(arg.config.gpio, function() {
-					 setGpioValue(arg.config.gpio,"value",arg.open?0:1);
-				});
-			 } 
+module.exports = function () {
+  this.init = function (arg) {
+    console.log("Start GPIO ...");
+    this.events.on('changeState', function (arg) {
+      if (arg.config.gpio) {
+        console.log('GPIO : open ' + arg.id + "=" + arg.open);
+        checkPin(arg.config.gpio, function () {
+          setGpioValue(arg.config.gpio, "value", arg.open ? 0 : 1);
+        });
+      }
 	   });
-	};
+  };
 };
