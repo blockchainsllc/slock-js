112efdce29cc8d4bb62e94e98f7051f99efcedad
Simon Jentzsch <simon@jentzschfamily.net>
2015-11-19 23:42:58 +0100

    Update slock-snap.js

--- bin/slock-snap.js
+++ bin/slock-snap.js
@@ -1,3 +1,3 @@
 #!bin/node
 
-require('./lib/node_modules/slock/src/config.js').start(process.argv[2]||'lib/node_modules/slock/conf/config_snap.json');
+require('../src/config.js').start(process.argv[2]||'lib/node_modules/slock/conf/config_snap.json');
