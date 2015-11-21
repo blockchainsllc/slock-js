1edfbec401ab476692e2ae483320c4b956109d8a
Simon Jentzsch <simon.jentzsch@slock.it>
2015-11-21 13:52:02 +0100

    changed values

--- modules/shh.js
+++ modules/shh.js
@@ -1,3 +1,11 @@
+function normalizeAdr(a) {
+   if (!a) return a;
+   if (a.length>2 && a[1]=='x') a=a.substring(2);
+   while (a.length<40) a="0"+a;
+   return "0x"+a;
+}
+
+
 module.exports = function () {
 
    // we use the events and config from the prototype, because the
@@ -11,9 +19,9 @@ module.exports = function () {
       
       // watch for incoming messages
       web3.shh.watch({ 
-         'topic': [ web3.fromAscii(contract.config.adr) ] 
+         'topic': [ normalizeAdr(contract.config.adr) ] 
       }).arrived(function(m) {
-         events.emit("message", { to: contract.config.adr, msg: web3.toAscii(m.payload[0]), from:web3.toAscii(m.payload[1])});
+         events.emit("message", { to: normalizeAdr(contract.config.adr), msg: web3.toAscii(m.payload[0]), from:m.payload[1] });
       });
       
    });
