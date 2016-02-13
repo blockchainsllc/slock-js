da7321330965ab9f6094d243dda1b4614e4e42b1
Simon Jentzsch <simon.jentzsch@slock.it>
2016-02-13 13:29:51 +0100

    fixed formating of the adr

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,7 @@ module.exports = {
 	normalizeAdr : function (a) {
 		if (!a) return a;
 		if (a.length>2 && a[1]=='x') a=a.substring(2);
+		while (a.length>0 && a.substring(0,2)=="00") a=a.substring(2);
 		while (a.length<40) a="0"+a;
 		return "0x"+a;
 	}
