119bd12f621d5eed7d43177e645d0db87137d111
Lefteris Karapetsas <lefteris@refu.co>
2016-02-13 18:35:10 +0100

    Styling in config.js

--- src/config.js
+++ src/config.js
@@ -3,43 +3,53 @@
 
 var fs   = require('fs');
 var core = require('./core.js');
+var yaml = require('js-yaml');
 
 function Config() {}
 Config.start = function(configFile) {
-	// read the config-file...
-	fs.readFile(configFile,'utf-8', function(err, data) {
-		// handle error
-		if (err) 
-			console.log("Could not read the "+configFile+"  :"+err);
-		else {
-			// start the application with the loaded config.
-		    var main = core(JSON.parse(data));
-		    
-			function reinit(err, data,count) {
-				if (err) return;
-				var conf={};
-				try {
-					conf = JSON.parse(data);
-				} catch (e) {
-					if (!count || count<5)
-						setTimeout(function(){fs.readFile(configFile,'utf-8', function(a1,a2) {reinit(a1,a2,(count||0)+1);});},500);
-					else
-					   console.log("Error trying to update from the config "+configFile);
-					return;
-				}
-				if (JSON.stringify(conf)==JSON.stringify(main.config)) return;
-		    	console.log("Config file ("+configFile+") changed. Reloading...");
-				main.oldConfig = main.config;
-				main.config    = conf;
-				main.events.emit("init",main);
-			}	
+    // read the config-file...
+    fs.readFile(configFile, 'utf-8', function(err, data) {
+        // handle error
+        if (err) {
+            console.log("Could not read the " + configFile + "  :"+err);
+        } else {
+            // start the application with the loaded config.
+            var main = core(JSON.parse(data));
 
-			// watch the config for changes and call reinit
-		    fs.watch(configFile,function (event, filename){
-		    	fs.readFile(configFile,'utf-8', reinit);
-		    });
-		}
-	});
+            function reinit(err, data, count) {
+                if (err) return;
+                var conf={};
+                try {
+                    conf = JSON.parse(data);
+                } catch (e) {
+                    if (!count || count < 5)
+                        setTimeout(
+                            function(){
+                                fs.readFile(configFile,
+                                            'utf-8',
+                                            function(a1, a2) {
+                                                reinit(a1, a2, (count || 0) + 1);
+                                            });
+                            },
+                            500
+                        );
+                    else
+                        console.log("Error trying to update from the config " + configFile);
+                    return;
+                }
+                if (JSON.stringify(conf) == JSON.stringify(main.config))
+                    return;
+                console.log("Config file ("+configFile+") changed. Reloading...");
+                main.oldConfig = main.config;
+                main.config    = conf;
+                main.events.emit("init",main);
+            }
+
+            // watch the config for changes and call reinit
+            fs.watch(configFile,function (event, filename){
+                fs.readFile(configFile,'utf-8', reinit);
+            });
+        }
+    });
 };
-
 module.exports = Config;
