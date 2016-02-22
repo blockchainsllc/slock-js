f4b1e7e7c0e3668940004e9013caa9aec7861ac0
Lefteris Karapetsas <lefteris@refu.co>
2016-02-22 22:43:18 +0100

    Remove default for zway
    Reasoning: default for 0 did not work

--- modules/zway.js
+++ modules/zway.js
@@ -1,23 +1,23 @@
 var http = require('http');
 
 module.exports = function() {
-		
+
    this.init = function(arg) {
 	  var config = arg.config.modules.zway;
      console.log("Init zway-connection ...");
-     
+
 	  var host = config.host || 'localhost';
 	  var port = config.port || 8083;
-	 
+
      this.events.on('changeState',function(arg) {
     	 var conf = arg.config;
-    	 var vOn  = conf.open || 0;
-    	 var vOff = conf.close || 255;
+    	 var vOn  = conf.open;
+    	 var vOff = conf.close;
        console.log('Zway : open '+arg.id+"="+arg.open);
     	 if (conf.nodeid) {
              var url = "http://"+host+":"+port+"/ZWaveAPI/Run/devices["+conf.nodeid+"].instances["+(conf.instance || 0)+"].commandClasses["+(conf.command || 98)+"].Set("+(arg.open?vOn:vOff)+")";
              console.log('Zway : open '+arg.id+"="+arg.open + " url:"+url);
-             
+
              http.get(url, function(res) {
             	  console.log("Got response: " + res.statusCode);
             	}).on('error', function(e) {
